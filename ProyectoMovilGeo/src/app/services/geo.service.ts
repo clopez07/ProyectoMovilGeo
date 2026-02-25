44ad2b6153364697dc70b105ba6532a4b81d91f5
Carlos Lopez <cilopez1@uth.hn>
2026-02-24 18:28:45 -0600

    "clase para geoservice.ts"

--- ProyectoMovilGeo/src/app/services/geo.service.ts
+++ ProyectoMovilGeo/src/app/services/geo.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import {Geolocation} from '@capacitor/geolocation';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GeoService {
   
+  async getCurrentPosition(): Promise<{lat: number; lng: number}>{
+    const pos = await Geolocation.getCurrentPosition({enableHighAccuracy: true});
+    return {lat: pos.coords.latitude, lng: pos.coords.longitude};
+  }
 }
