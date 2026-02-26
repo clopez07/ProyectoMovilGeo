e3e2f6cbefd688a88063deb6637280795468ae7c
Carlos Lopez <cilopez1@uth.hn>
2026-02-26 10:15:30 -0600

    API fotos-local

--- ProyectoMovilGeo/src/app/services/fotos-local.ts
+++ ProyectoMovilGeo/src/app/services/fotos-local.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Camera, CameraSource, CameraResultType } from '@capacitor/camera';
+import { Capacitor } from '@capacitor/core';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { FotoLocal } from '../models/lugares.models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FotosLocal {
   
+  async tomaroSeleccionarFoto(source: CameraSource): Promise<FotoLocal> {
+    const photo = await Camera.getPhoto({
+      quality: 85,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source
+    });
+
+    if (!photo.base64String) throw new Error('No Base64');
+
+    const idLocal = `IMG_${Date.now()}`;
+    const fileName = `${idLocal}.jpeg`;
+
+    await Filesystem.writeFile({ //acceder al archivo como tal
+      path: fileName,
+      data: photo.base64String,
+      directory: Directory.Data
+    });
+
+    const fileUrl = await Filesystem.getUri({
+      path: fileName,
+      directory: Directory.Data
+    });
+
+    const pathLocal = Capacitor.convertFileSrc(fileUrl.uri);
+
+    return {idLocal, pathLocal, createdAt: new Date().toUTCString()};
+  }
+
 }
